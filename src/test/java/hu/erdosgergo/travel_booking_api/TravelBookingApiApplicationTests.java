18db228c6c500b8cd1f70d4d0c90dd699432b4f2
gerierdos <gergokiraly2@gmail.com>
2026-04-15 13:33:44 +0200

    Init Travel proejct setup

---
+++ src/test/java/hu/erdosgergo/travel_booking_api/TravelBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.erdosgergo.travel_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
